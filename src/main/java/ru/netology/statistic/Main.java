84e8726efd1916943bd53912c8b406482ad4f3ce
Asja <anastasjamedved@gmail.com>
2025-03-14 15:03:43 +0300

    error correction

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -1,10 +1,10 @@
 package ru.netology.statistic;
 public class Main {
     public static void main(String[] args) {
-        StatisticsService service = new StatisticsService();
+        StatisticsService servise = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
 
-        System.out.println(servise.findMax(incomes));
+        System.out.println(servise.findMax(incomesInBillions));
     }
     }
