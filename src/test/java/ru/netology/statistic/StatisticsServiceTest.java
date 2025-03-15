bbb586a3e2651478b907c6dcedf2079339ff1855
Asja <anastasjamedved@gmail.com>
2025-03-15 12:29:07 +0300

    adding a test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,40 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxWhenMaxAtStart() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11};
+        long expected = 12;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenMaxInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {5, 8, 4, 20, 3, 8, 6, 11, 12, 7};
+        long expected = 20;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenMaxAtEnd() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {5, 8, 4, 3, 8, 6, 11, 7, 22};
+        long expected = 22;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
